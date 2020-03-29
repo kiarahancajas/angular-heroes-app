a992e35e2f67d98c5e012d10090db76bd7fe9cfd
Kiarah Ancajas <kancajas@companionprotect.com>
2020-03-29 20:56:58 +0800

    adding components and services

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Hero } from '../hero'; 
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service'
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  heroes: Hero[];
+  selectedHero: Hero;
+  
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+
+  constructor(private heroService: HeroService, private messageService: MessageService) {}
+
+  ngOnInit(): void { //life cycle hook
+    this.getHeroes();
+  }
+  
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroService: Selected hero id=${hero.id}`);
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
+}
